16311c7b04d4ad0bc17ad6d27a45b21c14319af0
Pablo Quiroga <pabloquiroga@outlook.com.br>
2024-03-07 19:55:32 -0300

    logica do jogo

---
+++ app.js
@@ -0,0 +1,52 @@
+function sortear() {
+
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let sortados = [];
+    let numero;
+
+    for (let index = 0; index < quantidade; index++) {
+        numero = obterNumeroAleatorio(de, ate)
+
+        while (sortados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
+
+        sortados.push(numero);
+    }
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sortados}</label>`;
+
+    alterarStatusBotao();
+}
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+
+    if (botao.classList.contains('container__botao-desabilitado')) {
+
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+
+    } else {
+
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value - '';
+    document.getElementById('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`;
+    alterarStatusBotao()
+}
